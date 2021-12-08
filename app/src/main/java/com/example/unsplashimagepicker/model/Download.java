79a2749c49004b6d26f2dd037c10bb058f39fe56
Reuven Bazak <ReuvenBazak.gmail.com>
2021-12-08 18:05:50 +0200

    Unsplash app

---
+++ app/src/main/java/com/example/unsplashimagepicker/model/Download.java
@@ -0,0 +1,11 @@
+package com.example.unsplashimagepicker.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Download {
+
+    @SerializedName("url")
+    @Expose
+    private String url;
+}
---
+++ app/src/main/java/com/example/unsplashimagepicker/util/Config.java
@@ -0,0 +1,6 @@
+package com.example.unsplashimagepicker.util;
+
+public class Config {
+    public static final String unsplash_access_key = "7mszpXRPZ_SAN3qbALolUI3-IPOkFTO0zmj1__FqKeQ";
+    public static final String BASE_URL_UNSPLASH = "https://api.unsplash.com/";
+}
